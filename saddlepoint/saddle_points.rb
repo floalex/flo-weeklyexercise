88b2f5c76c6e54c7ecde7de51f133f8f8f03d893
floalex <flo.liang@my.com>
2016-04-26 03:01:05 +0000

    Saddle Points Exe

---
+++ saddlepoint/saddle_points.rb
@@ -0,0 +1,25 @@
+class Matrix
+  
+  def initialize(matrix_string)
+    @matrix = matrix_string.split("\n")
+  end
+  
+  def rows
+    @matrix.map {|group| group.split.map(&:to_i)}
+  end
+  
+  def columns
+    row_first, *row_other = *rows 
+    row_first.zip(*row_other)
+  end
+  
+  def saddle_points
+    array = []
+    (0..(columns[0].size-1)).each do |r_index|
+      (0..(rows[0].size-1)).each do |c_index|
+        array << [r_index, c_index] if rows[r_index].max == columns[c_index].min
+      end
+    end
+    array
+  end
+end
