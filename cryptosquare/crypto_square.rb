072457b11e5df9005aad5b40151e80c237907b4c
floalex <flo.liang@my.com>
2016-05-05 08:54:33 +0000

    Add cypto square

---
+++ cryptosquare/crypto_square.rb
@@ -0,0 +1,40 @@
+class Crypto
+  attr_reader :str
+  
+  def initialize(str)
+    @str = str
+  end
+  
+  def normalize_plaintext
+    str.downcase.gsub(/\W/, '')
+  end
+  
+  def size
+    num = (normalize_plaintext.length**0.5).to_i
+    if num**2 < normalize_plaintext.length
+      num += 1
+    end
+    num
+  end
+  
+  def plaintext_segments
+    normalize_plaintext.split('').each_slice(size).map(&:join)
+  end
+  
+  def text_rectangle
+    seg_array = plaintext_segments.map(&:chars)
+    first_part, *remaining_part = *seg_array
+    first_part.zip(*remaining_part)
+  end
+  
+  def ciphertext
+    text_rectangle.join
+  end
+  
+  def normalize_ciphertext
+    text_rectangle.map do |row|
+      row.compact.join
+    end.join(' ')
+  end
+    
+end
--- pascaltri/pascals_triangle.rb
+++ pascaltri/pascals_triangle.rb
@@ -21,3 +21,30 @@ class Triangle
   end
   
 end
+
+# #20160430 live session
+# class Triangle
+#   def initialize(levels)
+#     @levels = levels
+#   end
+  
+#   def rows
+#     rows = [[1]] #rows[0] = [1]
+#     (1..(@levels-1)).each do |level|
+#       rows[level] = next_row_for(rows[level-1])
+#     end
+#     rows
+#   end
+  
+#   def next_row_for(current_row)
+#     result = []
+#     current_row.each_with_index do |number, idx|
+#       if idx == 0
+#         result << 1
+#       else
+#         result << number + current_row[idx-1]
+#       end
+#     end
+#     result << 1
+#   end
+# end
