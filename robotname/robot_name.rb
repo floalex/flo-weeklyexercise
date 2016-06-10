a6c2c1a329f94a9767ea3ea39bf10873b7ca53d2
floalex <flo.liang@my.com>
2016-06-10 19:46:30 +0000

    rewrite robot_name.rb to fix space

--- robotname/robot_name.rb
+++ robotname/robot_name.rb
@@ -1,31 +1,31 @@
 class Robot
-	@@stored_names = []
-	
-	def name
-		@name ||= create_name
-	end
-	
-	def reset
-		@name = nil
-	end
-	
-	def self.stored_names
-		@@stored_names
-	end
-	
-	private
-	
-	def create_name
-		letter = ""
-		2.times { letter += ('A'..'Z').to_a.sample }
-		number = rand(100..1000)
-		name = "#{letter}#{number}"
-		
-		if self.class.stored_names.include?(name)
-			create_name
-		else
-			self.class.stored_names << name
-			name
-		end
-	end
+  @@stored_names = []
+  
+  def name
+    @name ||= create_name
+  end
+  
+  def self.stored_names
+    @@stored_names
+  end
+  
+  def reset
+    @name = nil
+  end
+  
+  private
+  
+  def create_name
+    letters = ""
+    2.times { letters += ('A'..'Z').to_a.sample }
+    number = rand(100..1000)
+    name = "#{letters}#{number}"
+    
+    if self.class.stored_names.include?(name)
+      create_name
+    else
+      self.class.stored_names << name
+      name
+    end
+  end
 end
